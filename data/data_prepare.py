5588c8d6662a696bb35e0afedc4cf08d3067cefb
soap117 <asbljy@163.com>
2017-05-01 21:44:11 +0800

    Update data_prepare.py

--- data/data_prepare.py
+++ data/data_prepare.py
@@ -25,19 +25,16 @@ def convert_to(data_path, name):
     cols = OUTPUT_SIZE
     depth = DEPTH
     for ii in range(10):
-        writer = tf.python_io.TFRecordWriter(name + str(ii) + '.tfrecords_edge2shoes')
+        writer = tf.python_io.TFRecordWriter(name + str(ii) + '.tfrecords')
         len = os.listdir(data_path).__len__()
         size_each = int(len/10)
         for img_name in os.listdir(data_path)[ii * size_each: (ii + 1) * size_each]:
-            # 打开图片
             img_path = data_path + img_name
             img = Image.open(img_path)
             img = np.asarray(img)
             img = cv.resize(img,(256,128))
             img = Image.fromarray(np.uint8(img))
-            # 转化为字节
             img_raw = img.tobytes()
-            # 写入到 Example
             example = tf.train.Example(features=tf.train.Features(feature={
                 'height': _int64_feature(rows),
                 'width': _int64_feature(cols),
@@ -49,8 +46,8 @@ def convert_to(data_path, name):
 
 if __name__ == '__main__':
     current_dir = os.getcwd()
-    data_path = raw_input("Enter the original data location: ");
-    name = raw_input("Where you want to save the new data: ");
+    data_path = input("Enter the original data location: ");
+    name = input("Where you want to save the new data: ");
     start_time = time.time()
 
     print('Convert start')
